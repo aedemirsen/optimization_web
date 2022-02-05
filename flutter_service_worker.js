5a8b4bce43f183fe82ab3ed5470bf1c2cb16c5f4
aedemirsen <aedemirsen@gmail.com>
2022-02-06 00:00:56 +0300

    second

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "222ac3a7da29afe785616402461e5f14",
-"index.html": "05d1d8d6eb1007f5c01487dfeb83b429",
-"/": "05d1d8d6eb1007f5c01487dfeb83b429",
-"main.dart.js": "2c919999d8ec7c45f08ab7159bb36ca2",
+"index.html": "1e62d15d22340abd3e36a65f6df895ca",
+"/": "1e62d15d22340abd3e36a65f6df895ca",
+"main.dart.js": "fe971961c1a8117fb984059559d807cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
